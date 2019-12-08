34fbc8b927485821ad3bb8be5d96582255e6525c
wanabe <s.wanabe@gmail.com>
2019-12-08 19:27:07 +0900

    feat: add main .go file

---
+++ gitm.go
@@ -0,0 +1,23 @@
+package main
+import (
+    "fmt"
+    "encoding/hex"
+
+    "github.com/golang/protobuf/proto"
+
+    "./pb"
+)
+
+func main() {
+    commit := pb.Commit {
+        Object: &pb.Object {
+            Hash: "4df003e28a16e91e9667c7e6ea5852202820ac67",
+        },
+    }
+    out, err := proto.Marshal(&commit)
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Printf("%s\ndone.\n", hex.Dump(out))
+}
